f047a18ceaa6706c9d0f6d70c3fe30364f46664a
Allan Zaparoli <allanzaparoli@Allans-MacBook-Pro-2.local>
2021-10-25 14:21:22 -0300

    atualização

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,17 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(tecName) {
+  tecName = {
+   tec: ["React", "Jest", "HTML", "CSS", "JavaScript"],
+   nome: "Allan"
+  }
+  let ListaCompleta = tecName.tec.sort();
+    for(let i = 0; i < 5; i += 1) {
+     if(ListaCompleta[i]=== "") {
+       tecName = "Vazio!";
+     }
+    }
+    return tecName;
+  }
 
 // Desafio 11
 function generatePhoneNumber() {
